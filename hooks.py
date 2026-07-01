485b8d9a6a99e5fe92f40217d439b723cfa98140
Gazer-git1 <m.skoryk@gazer.com>
2026-07-01 12:38:00 +0300

    Add files via upload

---
+++ hooks.py
@@ -0,0 +1,12 @@
+from . import __version__ as app_version
+
+app_name = "gazer_custom"
+app_title = "Gazer Custom"
+app_publisher = "Gazer"
+app_description = "Custom UI enhancements"
+app_email = "admin@gazer.com"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_license = "MIT"
+
+app_include_js = ["search.js"]
